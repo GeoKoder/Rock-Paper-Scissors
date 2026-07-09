77c6d2033df590c2b5adcc48649f18082c09020a
GeoKoder <kodergeo@gmail.com>
2026-07-09 07:00:34 +0300

    Add buttons and event listeners These were added to have a better way of input than the prompt.

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
 function getComputerChoice() {
   const randomNo = Math.floor(Math.random() * 3);
   const choices = ["rock", "paper", "scissors"];
   return choices[randomNo];
 }
 
-function getHumanChoice() {
-  let userChoice = prompt("What is your choice: Rock, Paper or Scissors");
-  return userChoice.toLowerCase();
-}
+// Replaced by the buttons
+// function getHumanChoice() {
+//   let userChoice = prompt("What is your choice: Rock, Paper or Scissors");
+//   return userChoice.toLowerCase();
+// }
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice == "rock" && computerChoice == "scissors") {
     humanScore += 1;
     console.log(
-      "Human wins with " + humanChoice + " and score of " + humanScore
+      "Human wins with " + humanChoice + " and score of " + humanScore,
     );
   } else if (humanChoice == "scissors" && computerChoice == "paper") {
     humanScore += 1;
@@ -23,12 +28,12 @@ function playRound(humanChoice, computerChoice) {
   } else if (humanChoice == "paper" && computerChoice == "rock") {
     humanScore += 1;
     console.log(
-      "Human wins with " + humanChoice + " and score of " + humanScore
+      "Human wins with " + humanChoice + " and score of " + humanScore,
     );
   } else {
     computerScore += 1;
     console.log(
-      "Computer wins with " + computerChoice + " and score of " + computerScore
+      "Computer wins with " + computerChoice + " and score of " + computerScore,
     );
   }
 }
@@ -37,9 +42,24 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playGame() {
-  for (i = 1; i <= 5; i++) {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
+  rockBtn.addEventListener("click", ()=> {
+    playRound("rock", getComputerChoice())
+  })
+
+  paperBtn.addEventListener("click", ()=> {
+    playRound("paper", getComputerChoice())
+  })
+
+  scissorsBtn.addEventListener("click", ()=> {
+    playRound("scissors", getComputerChoice())
+  })
+
+  
+  // for (i = 1; i <= 5; i++) {
+  //   let humanSelection = getHumanChoice();
+  //   let computerSelection = getComputerChoice();
+  //   playRound(humanSelection, computerSelection);
+  // }
 }
+
+playGame();
